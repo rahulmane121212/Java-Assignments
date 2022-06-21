a99d0d087ed8fd215ee414c3d13b9e5fd9d445d2
rahulmane121212 <107107447+rahulmane121212@users.noreply.github.com>
2022-06-21 23:23:06 +0530

    Add files via upload

---
+++ assignments3Stringa/Length.java
@@ -0,0 +1,13 @@
+package assignments3Stringa;
+
+public class Length {
+
+	public static void main(String[] args) {
+		String str = "Hello World";
+		int count= str.length();
+		
+		System.out.println("Length of string: "+count);
+
+	}
+
+}
